80ef315412935d758f9a6e851c8592a15cde32af
Jordan Sedano <=>
2020-03-07 14:24:18 -0500

    JWT installed

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 use Illuminate\Http\Request;
 
 /*
@@ -12,7 +12,21 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+Route::group([
+
+    //'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
 });
